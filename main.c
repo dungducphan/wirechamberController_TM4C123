99bba9806f21bab6acdd2694bc7c72a75b6f0eae
dungducphan <brianp.dung@utexas.edu>
2022-05-13 13:52:27 -0500

    First commit.

---
+++ main.c
@@ -0,0 +1,120 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/debug.h"
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/timer.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
+
+
+//*****************************************************************************
+// SELECT BITS
+//*****************************************************************************
+uint32_t g_ui32SelectBits;
+
+//*****************************************************************************
+// The error routine that is called if the driver library encounters an error.
+//*****************************************************************************
+#ifdef DEBUG
+void
+__error__(char *pcFilename, uint32_t ui32Line) {}
+#endif
+
+//*****************************************************************************
+// The interrupt handler for the first timer interrupt.
+//*****************************************************************************
+void Timer0IntHandler(void) {
+    // Clear the timer interrupt.
+    ROM_TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+
+    // Reset SELECT bits
+    if (HWREGBITW(&g_ui32SelectBits, 5) == 1) {
+        g_ui32SelectBits = 0;
+    }
+
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, HWREGBITW(&g_ui32SelectBits, 0));
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, HWREGBITW(&g_ui32SelectBits, 1));
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, HWREGBITW(&g_ui32SelectBits, 2));
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, HWREGBITW(&g_ui32SelectBits, 3));
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_5, HWREGBITW(&g_ui32SelectBits, 4));
+
+    // Update select bits
+    g_ui32SelectBits++;
+}
+
+//*****************************************************************************
+// Configure the UART and its pins.  This must be called before UARTprintf().
+//*****************************************************************************
+void ConfigureUART(void) {
+    // Enable the GPIO Peripheral used by the UART.
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    // Enable UART0
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+
+    // Configure GPIO Pins for UART mode.
+    ROM_GPIOPinConfigure(GPIO_PA0_U0RX);
+    ROM_GPIOPinConfigure(GPIO_PA1_U0TX);
+    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    // Use the internal 16MHz oscillator as the UART clock source.
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+
+    // Initialize the UART for console I/O.
+    UARTStdioConfig(0, 115200, 16000000);
+}
+
+int main(void) {
+
+    // Initialize SelectBits
+    g_ui32SelectBits = 0;
+
+    // Enable lazy stacking for interrupt handlers.  This allows floating-point
+    // instructions to be used within interrupt handlers, but at the expense of
+    // extra stack usage.
+    ROM_FPULazyStackingEnable();
+
+    // Set the clocking to run PLL
+    ROM_SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+
+    // Initialize the UART and write status.
+    ConfigureUART();
+    UARTprintf("Hello, world!\n");
+
+    // Enable the GPIO port that is used for the on-board LED.
+    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+    // Enable the GPIO pins for the LED (PF1).
+    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1);
+    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
+    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
+    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_4);
+    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_5);
+
+    // Enable the peripherals used by this example.
+    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+
+    // Enable processor interrupts.
+    ROM_IntMasterEnable();
+
+    // Configure the two 32-bit periodic timers.
+    ROM_TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
+    ROM_TimerLoadSet(TIMER0_BASE, TIMER_A, ROM_SysCtlClockGet() / 8);
+
+    // Setup the interrupts for the timer timeouts.
+    ROM_IntEnable(INT_TIMER0A);
+    ROM_TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+
+    // Enable the timers and start infinite loop
+    ROM_TimerEnable(TIMER0_BASE, TIMER_A);
+    while(1) {}
+}
